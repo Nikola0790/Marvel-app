2bb8e4a9d27c212493b9f51de33a8e426261c06c
Nikola0790 <nikola.drcalic@gmail.com>
2021-09-03 18:41:58 +0200

    search_bar

---
+++ src/components/main_page/searchBar.jsx
@@ -0,0 +1,13 @@
+// SEARCH BAR
+
+import React from "react";
+const searchBar = () => {
+  return (
+    <div className="searchBox">
+      <input className="" type="text" placeholder="Search" />
+      <button>Go</button>
+    </div>
+  );
+};
+
+export default searchBar;
