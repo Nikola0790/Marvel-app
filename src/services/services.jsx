31eef67b6ed66a0e3707c9f69f8852f8f705a991
MiljanaB <miljana.brletic@gmail.com>
2021-09-04 16:08:47 +0200

    update searchBar and myTeam

--- src/services/services.jsx
+++ src/services/services.jsx
@@ -1,6 +1,5 @@
 const hash = "5d35920d8298a03cdd29556477f02a05";
-
-const heroCharacters = () => {
+export const heroCharacters = () => {
   return fetch(
     `http://gateway.marvel.com/v1/public/characters?ts=1&apikey=e680e5543fe28cd5b55866a7012dd7d9&hash=${hash}`
   )
@@ -11,5 +10,14 @@ const heroCharacters = () => {
       return res;
     });
 };
-
-export default heroCharacters;
+export const searchCharacter = (query) => {
+  return fetch(
+    `http://gateway.marvel.com/v1/public/characters?nameStartsWith=${query}&ts=1&apikey=e680e5543fe28cd5b55866a7012dd7d9&hash=${hash}`
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((result) => {
+      return result;
+    });
+};
